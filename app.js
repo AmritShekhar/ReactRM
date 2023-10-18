4ed540c3c0a2135b7c89aa8ea9e74f0103d60178
AmritShekhar <amritshekharrana@gmail.com>
2023-10-18 19:10:37 +0530

    commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const parent = React.createElement(
+
+    "div",
+
+    {
+        id: "parent"
+    },
+
+    [
+        React.createElement(
+
+            "div",
+
+            {
+                id: "child1"
+            },
+
+            [
+                React.createElement(
+
+                    "h1",
+
+                    {
+                        id: "child1h1"
+                    },
+
+                    "Child1's h1"
+                ),
+
+                React.createElement(
+
+                    "h2",
+
+                    {
+                        id: "child1h2"
+                    },
+
+                    "Child1's h2"
+                )
+            ]
+        ),
+
+        React.createElement(
+
+            "div",
+
+            {
+                id: "child2"
+            },
+
+            [
+
+                React.createElement(
+
+                    "h1",
+
+                    {
+                        id: "child2h1"
+                    },
+
+                    "Child2's h1"
+                ),
+
+                React.createElement(
+
+                    "h2",
+
+                    {
+                        id: "child2h2"
+                    },
+
+                    "Child2's h2"
+                )
+            ]
+        ),
+    ]
+);
+
+const commence = ReactDOM.createRoot(
+
+    document.getElementById("root")
+);
+
+commence.render(parent);
